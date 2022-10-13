48acfe53ca2fc1f32cd4369eccf762d608286e6e
Agnes Ogunmola <oladepe1103@gmail.com>
2022-10-13 03:53:23 -0700

    It prints exactly Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-
-
-
 /**
  * main - printing   
  *
  * Return: Always 0 (Successful)
 */
-
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
