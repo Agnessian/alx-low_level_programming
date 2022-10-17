571f67ed49de20021b439a3fbed95d7ae258d12f
Agnes Ogunmola <oladepe1103@gmail.com>
2022-10-17 14:04:30 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,10 @@ int main(void)
 	int num;
 	char la;
 
-	for (num = 0; num<10; num++)
+	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
-	for (la = 'a';  la >= 'f'; la--)
+	for (la = 'a';  la <= 'f'; la--)
 		putchar(la);
 
 	putchar('\n');
