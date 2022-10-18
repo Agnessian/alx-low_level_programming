4de69aa0ee180976cb5510b44717f5bb5c9d950b
Agnes Ogunmola <oladepe1103@gmail.com>
2022-10-18 13:01:53 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_putchar - writes the character c to stdout
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; laa++)
+		_putchar(la);
+
+	_putchar('\n');
+}
